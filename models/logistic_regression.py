c95f6420c75d0cc9ee3fb91560d5f2d661fbd1da
Breadfast <test@tests-MacBook-Pro-2.local>
2026-02-04 14:29:20 +0200

    Changing Logistic Regression Logic

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -8,46 +8,58 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score
 import numpy as np
 from tabulate import tabulate
+import joblib
+from pathlib import Path
 
 # Render input data
-ANCHOR_DATE = '2025-11-01'
-USE_CASE = "food"
-
+ANCHOR_DATE = '2025-12-01'
+USE_CASE = "beauty"
 
 
 df = render(anchor_date=ANCHOR_DATE,use_case=USE_CASE)
-print(df)
+X = df.drop(columns=["customer_id", "converted", "is_baby_parent", "is_pet_owner", 
+                        "office_ratio", "months_since_acquisition", "health_and_wellness_ratio"])
 
-# Create X and Y 
-X = df.drop(columns=["customer_id", "converted"])
 y= df["converted"]
+y_test = df["converted"]
+X_test = X
 
-pipe = Pipeline([ ("imputer", SimpleImputer(strategy="median")),
-                     ("scaler", StandardScaler()),
-                  ("model", LogisticRegression(max_iter=1000, class_weight="balanced"))])
-
-
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
-)
-
-pipe.fit(X_train,y_train)
+if (not Path("output/lr_propensity_model.pkl").exists()):
+# Create Pipeline and Save Model
+    pipe = Pipeline([ ("imputer", SimpleImputer(strategy="median")),
+                        ("scaler", StandardScaler()),
+                    ("model", LogisticRegression(max_iter=1000, class_weight="balanced"))])
 
 
-# Feature relevance
-coef_df = pd.DataFrame(
-    {"feature": X_train.columns, 
-     "coeff": pipe.named_steps["model"].coef_[0]
-    }
-)
 
-# Export feature coeffecients
-coef_df.to_csv("results/feature_coef_{{USE_CASE}}_{{ANCHOR_DATE}}.csv")
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42
+    )
+
+    pipe.fit(X_train,y_train)
+
+    # Feature relevance
+    coef_df = pd.DataFrame(
+        {"feature": X_train.columns, 
+        "coeff": pipe.named_steps["model"].coef_[0]
+
+        }
+    )
+    coef_df["odds_ratio"] = np.exp(coef_df["coeff"])
+
+    # Export feature coeffecients
+    coef_df.to_csv("results/lr_feature_coef.csv")
+
+else: 
+ # Load model
+    pipe = joblib.load("output/lr_propensity_model.pkl")
+    print("Model loaded from cache")
+
 
 # Prediction Accuracy 
 y_pred = pipe.predict(X_test)
 y_proba = pipe.predict_proba(X_test)[:,1]
+
 
 # Calculate Scores 
 acc = accuracy_score(y_test, y_pred)
@@ -58,18 +70,23 @@ comparison_df = pd.DataFrame({"actual": y_test.values, "prob": y_proba}).sort_va
 k = int(0.10*len(comparison_df))
 top_k= comparison_df.head(k)
 capture_rate = top_k["actual"].sum()/ comparison_df["actual"].sum()
+baseline_conversion = y_test.sum()/len(y_test)
 
+precision_at_k = top_k["actual"].mean()
 
 metrics = pd.DataFrame( [
     {"Accuracy": acc, 
      "AUC": auc, 
      "Precision": precision, 
      "Recall": recall, 
-     "Capture Rate (Recall @K =10%)": capture_rate
-     }
+     "Capture Rate (Recall @K =10%)": capture_rate,
+     "Baseline Conversion Rate": baseline_conversion,
+     "Precision at K": precision_at_k
+    }
 ]
 )
 print(tabulate(metrics, tablefmt="pretty", showindex=False, headers="keys"))
+joblib.dump(pipe, "output/lr_propensity_model.pkl")
 
 
 
--- render_query.py
+++ render_query.py
@@ -5,15 +5,19 @@ from pathlib import Path
 from configs.lookup import VERTICAL_LOOKUP
 
 def render(anchor_date, use_case):
-    project_id = "followbreadfast"
-    client = bigquery.Client(project=project_id)
+    if (not Path("input/cached_input_data_.csv").exists()):
+        project_id = "followbreadfast"
+        client = bigquery.Client(project=project_id)
 
-    env = Environment(loader=FileSystemLoader("templates"))
+        env = Environment(loader=FileSystemLoader("templates"))
 
-    template = env.get_template("query")
-    query = template.render(anchor_date = anchor_date, use_case_filter =VERTICAL_LOOKUP[use_case]) 
+        template = env.get_template("query")
+        query = template.render(anchor_date = anchor_date, use_case_filter =VERTICAL_LOOKUP[use_case]) 
 
-    customers = client.query(query).to_dataframe()
+        customers = client.query(query).to_dataframe()
 
-    customers.to_csv("input/")
+        customers.to_csv("input/cached_input_data.csv")
+    else: 
+        customers = pd.read_csv("input/cached_input_data.csv")
+
     return customers
